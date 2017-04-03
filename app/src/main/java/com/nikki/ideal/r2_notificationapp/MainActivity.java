dd5519566673b687d68b36bd90998f68349c8714
Rumani <rumanicharaya491@gmail.com>
2017-04-03 15:05:24 +0530

    Initial commit

---
+++ app/src/main/java/com/nikki/ideal/r2_notificationapp/MainActivity.java
@@ -0,0 +1,39 @@
+package com.nikki.ideal.r2_notificationapp;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button=(Button)findViewById(R.id.button);
+
+        Intent intent = new Intent(this, ResultActivity.class);
+        PendingIntent pIntent = PendingIntent.getActivity(this, (int) System.currentTimeMillis(), intent, 0);
+        Notification n  = new Notification.Builder(this)
+                .setContentTitle("New mail from " + "test@gmail.com")
+                .setContentText("Subject")
+                .setSmallIcon(R.drawable.notification_icon)
+                .setContentIntent(pIntent)
+                .setAutoCancel(true)
+                .addAction(R.drawable.notification_icon, "Call", pIntent)
+                .addAction(R.drawable.notification_icon, "More", pIntent)
+                .addAction(R.drawable.notification_icon, "And more", pIntent).build();
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0, n);
+    }
+
+}
+
+
+
---
+++ app/src/main/java/com/nikki/ideal/r2_notificationapp/ResultActivity.java
@@ -0,0 +1,20 @@
+package com.nikki.ideal.r2_notificationapp;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
+
+public class ResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+    }
+}
